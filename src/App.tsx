332720ec5d6dd294864776ebd86956a61bca0bbc
alveek <alveek@gmail.com>
2023-04-17 00:18:50 +0300

    fixing bugs

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,15 @@ import {
   Input,
   Heading,
   Box,
+  Button,
+  Text,
 } from '@chakra-ui/react';
 
 export default function App() {
-  type KoefType = number;
-
   const [resTotal, setResTotal] = useState(0);
   const [devTotal, setDevTotal] = useState(0);
+  const [results, setResults] = useState(0);
+  const [message, setMessage] = useState('');
   const [k1, setK1] = useState(0);
   const [k2, setK2] = useState(0);
   const [k3, setK3] = useState(0);
@@ -31,7 +33,7 @@ export default function App() {
   const getResourcesTotal = (): number => {
     let result = 0;
     if (k1 && k2 && k3 && k4) {
-      result = +(0.87 * k1 + 0.53 * k2 + 0.69 * k3 + 0.53 * k4).toFixed(2);
+      result = parseFloat((0.87 * k1 + 0.53 * k2 + 0.69 * k3 + 0.53 * k4).toFixed(2));
       return result;
     }
     return 0;
@@ -40,40 +42,68 @@ export default function App() {
   const getDevelopmentTotal = (): number => {
     let result = 0;
     if (k5 && k6 && k7) {
-      result = +(0.6 * k5 + 0.74 * k6 + 0.81 * k7).toFixed(2);
+      result = parseFloat((0.6 * k5 + 0.74 * k6 + 0.81 * k7).toFixed(2));
       return result;
     }
     return 0;
   };
 
   const getDiff = (): number => {
+    let result = 0;
     if (resTotal && devTotal) {
-      return devTotal / resTotal;
+      result = devTotal / resTotal;
+      setResults(result);
     }
     return 0;
+  };
+
+  const getResultsText = (value: number): string => {
+    if (value > 1) {
+      setMessage(
+        'Ресурсы и производственный потенциал предприятия используются эффективно',
+      );
+    } else if (value < 1) {
+      setMessage(
+        'Ресурсы и производственный потенциал предприятия используются неэффективно, необходима корректировка производственной политики',
+      );
+    }
+    return '';
   };
 
   useEffect(() => {
     setResTotal(getResourcesTotal);
     setDevTotal(getDevelopmentTotal);
+    getDiff();
   }, [k1, k2, k3, k4, k5, k6, k7]);
 
-  console.log(resTotal);
+  console.log(results);
   return (
-    <Container maxW="container.md">
-      <Heading as="h1" size="lg" textAlign="center" my={10}>
+    <Container
+      maxW='container.md'
+      display='flex'
+      flexDirection='column'
+      my={10}>
+      <Heading
+        as='h1'
+        size='lg'
+        textAlign='center'>
         Пояснение РИД Развитие и Ресурсы
       </Heading>
-
       <TableContainer>
-        <Table colorScheme="blue" size="sm">
-          <TableCaption placement="top" fontSize={18}>
+        <Table
+          colorScheme='blue'
+          size='sm'>
+          <TableCaption
+            placement='top'
+            fontSize={18}>
             Компонента "Ресурсы"
           </TableCaption>
           <Thead>
             <Tr>
-              <Th color="black">Показатель</Th>
-              <Th color="black" textAlign="center">
+              <Th color='black'>Показатель</Th>
+              <Th
+                color='black'
+                textAlign='center'>
                 Значение
               </Th>
             </Tr>
@@ -84,11 +114,11 @@ export default function App() {
               <Td isNumeric>
                 <Input
                   min={0}
-                  type="number"
-                  variant="outline"
-                  size="md"
-                  width="100px"
-                  textAlign="right"
+                  type='number'
+                  variant='outline'
+                  size='md'
+                  width='100px'
+                  textAlign='right'
                   height={7}
                   onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
                     console.log(e.target.value.length);
@@ -98,18 +128,16 @@ export default function App() {
               </Td>
             </Tr>
             <Tr>
-              <Td>
-                Доля внутренних затрат на исследования и разработки в выпуске, %
-              </Td>
+              <Td>Доля внутренних затрат на исследования и разработки в выпуске, %</Td>
               <Td isNumeric>
                 {' '}
                 <Input
                   min={0}
-                  type="number"
-                  variant="outline"
-                  size="md"
-                  width="100px"
-                  textAlign="right"
+                  type='number'
+                  variant='outline'
+                  size='md'
+                  width='100px'
+                  textAlign='right'
                   height={7}
                   onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                     setK2(Number(e.target.value))
@@ -123,11 +151,11 @@ export default function App() {
                 {' '}
                 <Input
                   min={0}
-                  type="number"
-                  variant="outline"
-                  size="md"
-                  width="100px"
-                  textAlign="right"
+                  type='number'
+                  variant='outline'
+                  size='md'
+                  width='100px'
+                  textAlign='right'
                   height={7}
                   onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                     setK3(Number(e.target.value))
@@ -140,11 +168,11 @@ export default function App() {
               <Td isNumeric>
                 <Input
                   min={0}
-                  type="number"
-                  variant="outline"
-                  size="md"
-                  width="100px"
-                  textAlign="right"
+                  type='number'
+                  variant='outline'
+                  size='md'
+                  width='100px'
+                  textAlign='right'
                   height={7}
                   onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                     setK4(Number(e.target.value))
@@ -153,31 +181,36 @@ export default function App() {
               </Td>
             </Tr>
           </Tbody>
-          <Tfoot>
+          {/* <Tfoot>
             <Tr>
               <Th>Итог</Th>
               <Th isNumeric>{resTotal}</Th>
             </Tr>
-          </Tfoot>
+          </Tfoot> */}
         </Table>
       </TableContainer>
-
-      <TableContainer maxW="780px">
-        <Table colorScheme="blue" size="sm">
-          <TableCaption placement="top" fontSize={18}>
+      <TableContainer
+        maxW='780px'
+        mb={10}>
+        <Table
+          colorScheme='blue'
+          size='sm'>
+          <TableCaption
+            placement='top'
+            fontSize={18}>
             Компонента "Развитие"
           </TableCaption>
           <Tbody>
             <Tr>
-              <Td width="462px">Индекс промышленного производства, %</Td>
+              <Td width='462px'>Индекс промышленного производства, %</Td>
               <Td isNumeric>
                 <Input
                   min={0}
-                  type="number"
-                  variant="outline"
-                  size="md"
-                  width="100px"
-                  textAlign="right"
+                  type='number'
+                  variant='outline'
+                  size='md'
+                  width='100px'
+                  textAlign='right'
                   height={7}
                   onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                     setK5(Number(e.target.value))
@@ -186,17 +219,15 @@ export default function App() {
               </Td>
             </Tr>
             <Tr>
-              <Td width="462px">
-                Индекс физического объема добавленной стоимости, %
-              </Td>
+              <Td width='462px'>Индекс физического объема добавленной стоимости, %</Td>
               <Td isNumeric>
                 <Input
                   min={0}
-                  type="number"
-                  variant="outline"
-                  size="md"
-                  width="100px"
-                  textAlign="right"
+                  type='number'
+                  variant='outline'
+                  size='md'
+                  width='100px'
+                  textAlign='right'
                   height={7}
                   onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                     setK6(Number(e.target.value))
@@ -205,15 +236,15 @@ export default function App() {
               </Td>
             </Tr>
             <Tr>
-              <Td width="462px">Структура ВДС добыча, %</Td>
+              <Td width='462px'>Структура ВДС добыча, %</Td>
               <Td isNumeric>
                 <Input
                   min={0}
-                  type="number"
-                  variant="outline"
-                  size="md"
-                  width="100px"
-                  textAlign="right"
+                  type='number'
+                  variant='outline'
+                  size='md'
+                  width='100px'
+                  textAlign='right'
                   height={7}
                   onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                     setK7(Number(e.target.value))
@@ -223,17 +254,30 @@ export default function App() {
             </Tr>
           </Tbody>
           <Tfoot>
-            <Tr>
+            {/* <Tr>
               <Th>Итог</Th>
               <Th isNumeric>{devTotal}</Th>
-            </Tr>
+            </Tr> */}
             <Tr>
               <Th>Соотношение Развитие / Ресурсы</Th>
-              <Th isNumeric>{getDiff()}</Th>
+              <Th isNumeric>{results.toFixed(2)}</Th>
             </Tr>
           </Tfoot>
         </Table>
       </TableContainer>
+      <Button
+        alignSelf='end'
+        onClick={() => {
+          setResults(getDiff);
+          setMessage(getResultsText);
+        }}>
+        Рассчитать
+      </Button>
+
+      <Box>
+        <Heading as='h3'>Выводы</Heading>
+        <Text>{message}</Text>
+      </Box>
     </Container>
   );
 }
