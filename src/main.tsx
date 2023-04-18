547c1507df72f1eafac340fe2069e50d7150eeab
alveek <alveek@gmail.com>
2023-04-18 16:30:50 +0300

    delete some files

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ChakraProvider>
